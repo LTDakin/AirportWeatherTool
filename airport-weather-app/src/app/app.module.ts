9c40cd4d9adca0d8c0161845e68cd9f3a26cd16a
LTDakin <taidakin99lol@gmail.com>
2023-08-04 13:45:22 -0700

    added forecasts

--- airport-weather-app/src/app/app.module.ts
+++ airport-weather-app/src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- airport-weather-app/src/app/components/home/home.component.ts
+++ airport-weather-app/src/app/components/home/home.component.ts
@@ -5,20 +5,21 @@ import { Convert, FinalReport } from 'src/app/models/Report';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent{
+export class HomeComponent {
   // data fields
   airportID: string = '';
-  myData : FinalReport | undefined;
+  myData: FinalReport | undefined;
   airports: string[] = [];
 
   constructor(private dataService: DataService) {}
-  
+
   // add airport to the list of airports and clear the input box for the next name
-  public addAirport(){
+  public addAirport() {
     this.dataService.getData(this.airportID).subscribe((data) => {
       this.myData = data as FinalReport;
-    })
+      console.log(this.myData.Forecast);
+    });
   }
 }
